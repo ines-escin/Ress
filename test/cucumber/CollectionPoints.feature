df8e93e38d7f4b0a7a27d7329903f4b026d0a568
las33 <lpnsdnsp13>
2016-10-21 21:31:50 -0300

    Implementação dos cenarios de controlador

--- test/cucumber/CollectionPoints.feature
+++ test/cucumber/CollectionPoints.feature
@@ -5,30 +5,30 @@ Feature: Map Visualization
 
   #GUI
 
-  Scenario: Visualizar locais
-    Given Estou na página de visualização de mapa do ResS
-    And restaurantes “r1” e “r2” possuem coletas pendentes
-    When eu solicito a visualização das coletas
-    Then eu vejo a localização dos restaurantes  “r1” e “r2” no mapa da UFPE
-
-  Scenario: Sem coletas pendentes
-    Given Estou na página de visualização de mapa do ResS
-    And não existem coletas pendentes
-    When eu solicito a visualização das coletas
-    Then eu vejo uma mensagem sinalizando que não há restaurantes com coletas pendentes
+#  Scenario: Visualizar locais
+#    Given Estou na página de visualização de mapa do ResS
+#    And os restaurantes de login "r1" e "r2" possuem coletas pendentes
+#    When eu solicito a visualização das coletas
+#    Then eu vejo a localização dos restaurantes de login "r1" e "r2" no mapa da UFPE
+#
+#  Scenario: Sem coletas pendentes
+#    Given Estou na página de visualização de mapa do ResS
+#    And não existem locais com coletas pendentes
+#    When eu solicito a visualização das coletas
+#    Then eu vejo uma mensagem sinalizando que não há restaurantes com coletas pendentes
 
 
  #Controller
 
-  Scenario: Buscar endereços de coleta
-    Given existem coletas pendentes
-    When eu solicito os locais de coletas pendentes
-    Then o sistema retorna os endereços de coletas pendentes
+  Scenario: Buscar endereços de coleta pendentes
+    Given o local de login "ru" possui coletas pendentes
+    When eu solicito os enderecos dos locais com coletas pendentes
+    Then o sistema retorna os enderecos do locais de login "ru"
 
 
-  Scenario: Buscar endereços de coleta
+  Scenario: busca de enderecos nao havendo coletas pendentes
     Given não existem coletas pendentes
-    When eu solicito os locais de coletas pendentes
+    When  eu solicito os enderecos dos locais com coletas pendentes
     Then o sistema nada retorna
 
 
--- test/cucumber/steps/CollectionPointsSteps.groovy
+++ test/cucumber/steps/CollectionPointsSteps.groovy
@@ -1,31 +1,102 @@
+package cucumber.steps
+
 import br.ufpe.cin.ines.ress.PickupRequest
+import br.ufpe.cin.ines.ress.User
+import br.ufpe.cin.ines.ress.residuecollector.CollectorDashboardController
+import cucumber.api.PendingException
+import pages.CollectorDashboardPage
+import pages.HomePage
+import pages.LoginAuthenticationPage
 import pages.MapsPage
 
-import static cucumber.api.groovy.EN.*
+import static cucumber.api.groovy.EN.*;
+
+
 /**
- * Created by Leonardo on 15/10/2016.
+ * Created by Leonardo on 20/10/2016.
  */
 
+Given(~/^Estou na página de visualização de mapa do ResS$/) { ->
+    to HomePage
+    at HomePage
+    page.clickLogIn()
+    at LoginAuthenticationPage
+    page.fillUsername("admin")
+    page.fillPassword("pass")
+    page.logInButtonClick()
+    at CollectorDashboardPage
+    page.maps()
+    at MapsPage
+}
+And(~/^os restaurantes de login "([^"]*)" e "([^"]*)" possuem coletas pendentes$/) { String arg1, String arg2 ->
+    User user = User.findByUsername(arg1)
+    assert user != null
+    assert PickupRequest.findByGenerator(user)
 
-Given(~'^Estou na página de visualização de mapa do ResS$') { ->
-     to MapsPage
-     at MapsPage
+    user = User.findByUsername(arg2)
+    assert user != null
+    assert PickupRequest.findByGenerator(user)
+}
+When(~/^eu solicito a visualização das coletas$/) { ->
+    at MapsPage
+    page.clickCollectionPoints()
+}
+Then(~/^eu vejo a localização dos restaurantes de login "([^"]*)" e "([^"]*)" no mapa da UFPE$/) { String arg1, String arg2 ->
+    at CollectionPointsPage
+    page.hasmap()
 }
 
-And(~'não existem coletas pendentes$'){
-     assert PickupRequest.findAllByStatus(false).empty
-}
-
-When(~'eu solicito a visualização das coletas$'){
-     at MapsPage
-     page.clickCollectionPoints()
-}
-
-Then (~'eu vejo uma mensagem sinalizando que não há restaurantes com coletas pendentes$'){
-     at MapsPage
-     assert page.hasNoCollectionPoints()
+And(~/^não existem locais com coletas pendentes$/) { ->
+    assert PickupRequest.findAllByStatus(false).empty
 }
 
 
+Then(~/^eu vejo uma mensagem sinalizando que não há restaurantes com coletas pendentes$/) { ->
+    at MapsPage
+    assert page.hasNoCollectionPoints()
+}
+
+
+
+Given(~/^o local de login "([^"]*)" possui coletas pendentes$/) { String arg ->
+    def pickupRequest = new PickupRequest()
+
+    pickupRequest.generator = User.findByUsername(arg)
+    pickupRequest.date = new Date()
+    pickupRequest.status = false
+    pickupRequest.residueAmount = 40
+    pickupRequest.collector = User.findByUsername('admin')
+    pickupRequest.save()
+
+
+    def coletas = PickupRequest.findAllByStatus(false);
+    def generators = coletas.collect {it -> it.generator.username}
+    def nome = generators.findAll{it -> it == arg}
+
+    assert !nome.isEmpty()
+
+
+
+}
+
+def address
+When(~/^eu solicito os enderecos dos locais com coletas pendentes$/) { ->
+    def controlador = new CollectorDashboardController()
+    address = controlador.collectionPointsAddress()
+}
+
+Then(~/^o sistema retorna os enderecos do locais de login "([^"]*)"$/) { String arg ->
+    def usernames = address.collect{at -> at.user.username}
+    assert usernames.contains(arg)
+    PickupRequest.executeUpdate('delete from PickupRequest')
+}
+
+Given(~/^não existem coletas pendentes$/) { ->
+    assert PickupRequest.findAllByStatus(false).empty
+}
+
+Then(~/^o sistema nada retorna$/) { ->
+    assert address.empty
+}
 
 
