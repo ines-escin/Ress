7a659cf9948bf93ebf9063b3bd4aa69e09757757
Pedro Lins <plal@cin.ufpe.br>
2016-11-04 22:38:22 -0300

    atualizando testes, incompleto

--- test/cucumber/steps/RouteVisualizationSteps.groovy
+++ test/cucumber/steps/RouteVisualizationSteps.groovy
@@ -1,27 +1,46 @@
-import
-import pages.MapsPage
 
-...
+import br.ufpe.cin.ines.ress.PickupRequest
+import pages.CollectorDashboardPage
+import pages.HomePage
+import pages.LoginAuthenticationPage
+import pages.MapsPage
+import pages.RouteVisualizationPage
+
 
 Given(~'^Eu estou na tela de visualização de mapas') { ->
-    to MapsPage
+    to HomePage
+    at HomePage
+    page.clickLogIn()
+    at LoginAuthenticationPage
+    page.fillUsername("admin")
+    page.fillPassword("pass")
+    page.logInButtonClick()
+    at CollectorDashboardPage
+    page.maps()
     at MapsPage
 }
 
-And(~'^Existem coletas pendentes para a empresa "([^"]*)" nos pontos "([^"]*)" e "([^"]*)"$') {
-    at MapsPage
+And(~'^Existem coletas pendentes para a empresa "([^"]*)" nos pontos "([^"]*)" e "([^"]*)"$') { String arg1, String arg2 ->
+    User user = User.findByUsername(arg1)
+    assert user != null
+    assert PickupRequest.findByGenerator(user)
+
+    user = User.findByUsername(arg2)
+    assert user != null
+    assert PickupRequest.findByGenerator(user)
 }
 
 And(~'^Nao existem coletas pendentes') {
-    at MapsPage
+    assert PickupRequest.findAllByStatus(false).empty
 }
 
 Then(~'^O mapa mostra a rota "([^"]*)", passando pelos pontos "([^"]*)" e "([^"]*)"$') {
-
+    at RouteVisualizationPage
+    page.hasRoute()
 }
 
 Then(~'^O mapa exibe uma mensagem de erro$') {
-
+    at MapsPage
 }
 
 Given(~'^Existem coletas pendentes$') {
@@ -33,7 +52,8 @@ Given(~'^Nao existem coletas pendentes$') {
 }
 
 When(~'^Eu solicito a rota para as coletas pendentes$') {
-
+    at MapsPage
+    page.ClickRouteVisualization()
 }
 
 Then(~'^O sistema retorna a rota para as coletas pendentes') {
