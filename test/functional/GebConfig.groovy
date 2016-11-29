1c279d292c1560b10d6da83331b2b14a0255dc9d
plal <plal@cin.ufpe.br>
2016-11-29 17:13:34 -0300

    [#129707509] mudando gebConfig para rodar o travis, desfeito acidentalmente no commit anterior

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,17 +1,17 @@
+//import org.openqa.selenium.chrome.ChromeDriver
+//
+//driver = {
+//    File file = new File("chromedrivers/chromedriver.exe");
+//
+//
+//    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+//    new ChromeDriver();
+//}
+//
+//baseUrl = "http://localhost:8070/"
+
+
 import org.openqa.selenium.chrome.ChromeDriver
-
-driver = {
-    File file = new File("chromedrivers/chromedriver.exe");
-
-
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    new ChromeDriver();
-}
-
-baseUrl = "http://localhost:8070/"
-
-
-/*import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 
 environments {
@@ -30,4 +30,4 @@ environments {
     firefox {
         driver = { new FirefoxDriver() }
     }
-}*/
+}
