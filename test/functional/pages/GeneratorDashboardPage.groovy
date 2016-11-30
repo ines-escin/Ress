b588c4470ad79185dcc09adba136409a14f69684
Caio <caiorochapstrlrd@gmail.com>
2016-11-30 11:31:11 -0300

    finalização testes travis

--- test/functional/pages/GeneratorDashboardPage.groovy
+++ test/functional/pages/GeneratorDashboardPage.groovy
@@ -10,6 +10,7 @@ class GeneratorDashboardPage extends Page {
     static url = "generatorDashboard/"
 
     static at = {
+
         InternationalizationHelper helper = InternationalizationHelper.instance
         String GeneratorDashboard = "Dashboard"
         String  GeneratorTitle = helper.getMessage("default.page.title.dashboard", GeneratorDashboard)
