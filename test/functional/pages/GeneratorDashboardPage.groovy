5f573df4f276fd6c82e5ae6584ae76e35f08f192
Caio <caiorochapstrlrd@gmail.com>
2016-11-30 10:56:14 -0300

    terceiro teste do travis

--- test/functional/pages/GeneratorDashboardPage.groovy
+++ test/functional/pages/GeneratorDashboardPage.groovy
@@ -1,16 +1,20 @@
 package pages
 
 import geb.Page
+import steps.InternationalizationHelper
 
 /**
  * Created by user on 27/10/2015.
  */
 class GeneratorDashboardPage extends Page {
-    def titulo = "ResS - Dashboard"
-    static url = "ResS/generatorDashboard/"
+    static url = "generatorDashboard/"
 
     static at = {
-        title ==~ titulo
+        InternationalizationHelper helper = InternationalizationHelper.instance
+        String GeneratorDashboard = "Dashboard"
+        String  GeneratorTitle = helper.getMessage("default.page.title.dashboard", GeneratorDashboard)
+
+        title ==~ GeneratorTitle
     }
 
     def logOff(){
